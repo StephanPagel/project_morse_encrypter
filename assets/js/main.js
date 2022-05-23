80ff3cba0d26a7ea9d9117971ffe07b109d501f6
StephanPagel <stephan.pagel84@gmail.com>
2022-05-23 17:44:33 +0200

    finished

---
+++ assets/js/main.js
@@ -0,0 +1,54 @@
+let morseAlphabet = [
+    { letter: "1", morseCode: ".----" },
+    { letter: "2", morseCode: "..---" },
+    { letter: "3", morseCode: "...--" },
+    { letter: "4", morseCode: "....-" },
+    { letter: "5", morseCode: "....." },
+    { letter: "6", morseCode: "-...." },
+    { letter: "7", morseCode: "--..." },
+    { letter: "8", morseCode: "---.." },
+    { letter: "9", morseCode: "----." },
+    { letter: "0", morseCode: "-----" },
+    { letter: " ", morseCode: "    " },
+    { letter: "A", morseCode: ".-" },
+    { letter: "B", morseCode: "-..." },
+    { letter: "C", morseCode: "-.-." },
+    { letter: "D", morseCode: "-.." },
+    { letter: "E", morseCode: "." },
+    { letter: "F", morseCode: "..-." },
+    { letter: "G", morseCode: "--." },
+    { letter: "H", morseCode: "...." },
+    { letter: "I", morseCode: ".." },
+    { letter: "J", morseCode: ".---" },
+    { letter: "K", morseCode: "-.-" },
+    { letter: "L", morseCode: ".-.." },
+    { letter: "M", morseCode: "--" },
+    { letter: "N", morseCode: "-." },
+    { letter: "O", morseCode: "---" },
+    { letter: "P", morseCode: ".--." },
+    { letter: "Q", morseCode: "--.-" },
+    { letter: "R", morseCode: ".-." },
+    { letter: "S", morseCode: "..." },
+    { letter: "T", morseCode: "-" },
+    { letter: "U", morseCode: "..-" },
+    { letter: "V", morseCode: "...-" },
+    { letter: "W", morseCode: ".--" },
+    { letter: "X", morseCode: "-..-" },
+    { letter: "Y", morseCode: "-.--" },
+    { letter: "Z", morseCode: "--.." }
+];
+let input = document.getElementById("input");
+let output = document.getElementById("output");
+let outputArray = [];
+
+encryptText = () => {
+    let inputArray = input.value.toUpperCase().split('');
+    inputArray.map(letter => {
+        for (x = 0; x < morseAlphabet.length; x++)
+            if (letter == morseAlphabet[x].letter) {
+                outputArray.push(morseAlphabet[x].morseCode);
+            }
+    })
+    output.innerHTML = outputArray.join('');
+    output.style.fontSize = '30px';
+}
